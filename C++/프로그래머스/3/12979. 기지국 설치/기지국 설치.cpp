1dd1956432443a901bcea231ac9f880a4827d920
Soggeum <166914281+Soggeum@users.noreply.github.com>
2025-09-20 00:42:51 +0900

    [level 3] Title: 기지국 설치, Time: 0.09 ms, Memory: 3.95 MB -BaekjoonHub

--- C++/프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
+++ C++/프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
@@ -5,24 +5,20 @@
 
 using namespace std;
 
-int solution(int N, vector<int> stations, int W) {
-  int answer = 0;
-  int location = 1; // 현재 탐색하는 아파트의 위치
-  int idx = 0; // 설치된 기지국의 인덱스
-
-  while (location <= N) {
-    // 기지국이 설치된 위치에 도달한 경우
-    if (idx < stations.size() && location >= stations[idx] - W) {
-      location = stations[idx] + W + 1;
-      idx++;
+int solution(int n, vector<int> stations, int w)
+{
+    int answer = 0;
+    int unWifi = 1;
+    float needed;
+    for (int i = 0; i < stations.size(); i++) {
+        needed = (float)((stations[i] - w - 1) - unWifi + 1) / (2 * w + 1);
+        answer += max((float)0.0, ceil(needed));
+        unWifi = stations[i] + w + 1;
     }
-    // 기지국이 설치되지 않은 위치인 경우
-    else {
-      // 기지국을 설치하고 해당 범위를 넘어감
-      location += 2 * W + 1;
-      answer++;
+    if (unWifi <= n) {
+        needed = (float)(n - unWifi + 1) / (2 * w + 1);
+        answer += ceil(needed);
     }
-  }
-
-  return answer;
+    
+    return answer;
 }
