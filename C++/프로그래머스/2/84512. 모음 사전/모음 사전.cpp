678de499381379d7a7d2df89976d975374787e67
Soggeum <166914281+Soggeum@users.noreply.github.com>
2025-10-14 13:59:08 +0900

    [level 2] Title: 모음 사전, Time: 0.03 ms, Memory: 3.84 MB -BaekjoonHub

--- C++/프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ C++/프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -1,17 +1,12 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
 #include <cmath>
-
-#include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 int PreWordCnt(char first_char, int num) {
-    static unordered_map<char, int> order = {{'A', 0},{'E', 1},{'I', 2},{'O',3},{'U', 4}};
-    if (num == 0)
-        return order[first_char];
-    
+    static unordered_map<char, int> order = {{'A', 0},{'E', 1},{'I', 2},{'O',3},{'U', 4}};    
     int result = 1;
     for (int i = 1; i <= num; i++) {
         result += pow(5, i);
@@ -24,7 +19,6 @@ int solution(string word) {
     for (int i = 0; i < word.length(); i++) {
         answer += PreWordCnt(word[i], 5 - i - 1);
         answer++;
-        cout << answer << endl;
     }
     return answer;
 }
