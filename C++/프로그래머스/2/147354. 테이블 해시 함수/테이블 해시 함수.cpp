5d213af80224752d64b5332b6b46b2d6cefb04ca
Soggeum <166914281+Soggeum@users.noreply.github.com>
2025-12-09 11:16:36 +0900

    [level 2] Title: 테이블 해시 함수, Time: 4.54 ms, Memory: 57.4 MB -BaekjoonHub

--- C++/프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
+++ C++/프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -12,12 +12,6 @@ int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
              return a[0] > b[0];
          });
     
-    // for (int i = 0; i < data.size(); i++) {
-    //     for (int j = 0; j < data[0].size(); j++)
-    //         cout << data[i][j] << " ";
-    //     cout << endl;
-    // }
-    
     int answer = 0;
     while (row_begin <= row_end) {
         int left = 0;
@@ -25,11 +19,7 @@ int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
             left += num % row_begin;
         }
         
-        if (answer == 0)
-            answer = left;
-        else {
-            answer ^= left;
-        }
+        answer ^= left;
         row_begin++;
     }
     
