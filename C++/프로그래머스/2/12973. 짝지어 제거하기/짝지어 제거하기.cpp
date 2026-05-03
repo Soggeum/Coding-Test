ec54ae4737e73b358be9554ab939adfe8ea3515c
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-05-03 15:35:40 +0900

    [level 2] Title: 짝지어 제거하기, Time: 6.74 ms, Memory: 6.29 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<string>
+#include <stack>
+
+using namespace std;
+
+int solution(string s)
+{
+    stack<char> st;
+    for (char c : s)
+    {
+        if (st.empty() || st.top() != c)
+        {
+            st.push(c);
+        }
+        else
+        {
+            st.pop();
+        }
+    }
+
+    return st.empty() ? 1 : 0;
+}
