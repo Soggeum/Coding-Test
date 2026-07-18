1cfcd433502976fd9c8fbaed20d0c5bb0727e274
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-18 23:55:58 +0900

    [level 2] Title: 가장 큰 수, Time: 80.89 ms, Memory: 7.8 MB -BaekjoonHub

--- C++/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ C++/프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -4,27 +4,22 @@
 
 using namespace std;
 
-struct comp
+bool comp(const int& a, const int& b)
 {
-    bool operator()(const int& a, const int& b) const
-    {
-        string A = to_string(a), B = to_string(b);
-        return A + B > B + A;
-    }  
-};
+    return to_string(a) + to_string(b) > to_string(b) + to_string(a);
+}
 
 string solution(vector<int> numbers) {
-    sort(numbers.begin(), numbers.end(), comp());
-    if (numbers[0] == 0)
+    sort(numbers.begin(), numbers.end(), comp);
+    string answer = "";
+    for (int number : numbers)
+    {
+        answer.append(to_string(number));
+    }
+    
+    if (answer[0] == '0')
     {
         return "0";
     }
-    
-    string answer = "";
-    for (int n : numbers)
-    {
-        answer.append(to_string(n));
-    }
-    
     return answer;
 }
