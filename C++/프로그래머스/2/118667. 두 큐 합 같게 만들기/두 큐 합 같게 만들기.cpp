bcef9fa66c65e83cdb644879d466c322e2e28384
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-19 00:16:20 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 5.35 ms, Memory: 27.8 MB -BaekjoonHub

--- C++/프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
+++ C++/프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -4,42 +4,55 @@
 using namespace std;
 
 int solution(vector<int> queue1, vector<int> queue2) {
+    vector<int> q;
     long long Sum = 0;
-    for (int i = 0; i < queue1.size(); i++) {
-        Sum += queue1[i] + queue2[i];
+    for (int n : queue1)
+    {
+        q.push_back(n);
+        Sum += n;
+        
     }
+    for (int n : queue2)
+    {
+        q.push_back(n);
+        Sum += n;
+    }
+    for (int n : queue1)
+    {
+        q.push_back(n);
+    }
+    
     if (Sum & 1)
+    {
         return -1;
-    Sum /= 2;
-    
-    int l = 0, r = queue1.size() - 1;
-    vector<int> q = queue1;
-    for (int num : queue2) {
-        q.push_back(num);
     }
-    long long s = 0;
-    for (int num : queue1) {
-        q.push_back(num);
-        s += num;
-    }
+    Sum = Sum >> 1;
     
-    int answer = 0;
-    while (r <= q.size() - 1) {
-        if (s == Sum)
-            return answer;
-        if (s < Sum) {
+    int answer = 0, Start = 0, End = queue1.size();
+    long long CurrSum = 0;
+    for (int i = Start; i < End; i++)
+    {
+        CurrSum += q[i];
+    }
+    while (Start <= End && End <= q.size())
+    {
+        if (CurrSum > Sum)
+        {
+            CurrSum -= q[Start++];
             answer++;
-            r++;
-            if (r == q.size())
-                return -1;
-            s += q[r];
         }
-        else {
-            answer++;
-            s -= q[l];
-            l++;
-            if (l == q.size())
+        else if (CurrSum < Sum)
+        {
+            if (End == q.size())
+            {
                 return -1;
+            }
+            CurrSum += q[End++];
+            answer++;
+        }
+        else
+        {
+            return answer;
         }
     }
     return -1;
