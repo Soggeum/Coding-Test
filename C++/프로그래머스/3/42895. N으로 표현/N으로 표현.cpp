b4f5d182ab49a851e95b0a0439b828ac5503dd5e
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-20 22:37:45 +0900

    [level 3] Title: N으로 표현, Time: 2.56 ms, Memory: 4.89 MB -BaekjoonHub

--- C++/프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ C++/프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -5,51 +5,54 @@
 using namespace std;
 
 int solution(int N, int number) {
-    vector<unordered_set<int>> DP(9);    
+    vector<unordered_set<int>> DP(9);
     for (int i = 1; i <= 8; i++)
     {
-        int Base = stoi(string(i, '0' + N));
-        if (Base == number)
+        int Num = stoi(string(i, '0' + N));
+        if (Num == number)
         {
             return i;
         }
-        DP[i].insert(Base);
-        
+        DP[i].insert(Num);
+    }
+    
+    for (int i = 2; i <= 8; i++)
+    {
         for (int j = 1; j < i; j++)
         {
             for (int op1 : DP[j])
             {
-                for (int op2 : DP[i - j])
+                for (int op2 : DP[i -j])
                 {
-                    int NewNumber = op1 + op2;
-                    DP[i].insert(NewNumber);
-                    if (NewNumber == number)
+                    int Num = op1 + op2;
+                    if (Num == number)
                     {
                         return i;
                     }
+                    DP[i].insert(Num);
                     
-                    NewNumber = op1 - op2;
-                    DP[i].insert(NewNumber);
-                    if (NewNumber == number)
+                    Num = op1 * op2;
+                    if (Num == number)
                     {
                         return i;
                     }
+                    DP[i].insert(Num);
                     
-                    NewNumber = op1 * op2;
-                    DP[i].insert(NewNumber);
-                    if (NewNumber == number)
+                    Num = op1 - op2;
+                    if (Num == number)
                     {
                         return i;
                     }
+                    DP[i].insert(Num);
                     
                     if (op2 != 0)
                     {
-                        NewNumber = op1 / op2;
-                        DP[i].insert(NewNumber);
-                        if (NewNumber == number)
+                        Num = op1 / op2;
+                        if (Num == number)
                         {
                             return i;
                         }
+                        DP[i].insert(Num);
                     }
                 }
             }
