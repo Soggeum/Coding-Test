539acad1c2c01706bbf05086af29be782b110a36
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-20 22:23:27 +0900

    [level 4] Title: 징검다리, Time: 4.41 ms, Memory: 5.32 MB -BaekjoonHub

--- C++/프로그래머스/4/43236. 징검다리/징검다리.cpp
+++ C++/프로그래머스/4/43236. 징검다리/징검다리.cpp
@@ -6,36 +6,37 @@ using namespace std;
 
 int solution(int distance, vector<int> rocks, int n) {
     sort(rocks.begin(), rocks.end());
-    int start = 1, end = distance, answer = 0;
-    while (start <= end)
+    
+    int answer = 1000000000, Start = 1, End = distance;
+    while (Start <= End)
     {
-        int mid = (start + end) / 2;
-        int rock = 0, LastRock = 0;
+        int mid = (Start + End) / 2, LastRock = 0, RemoveCount = 0;
         for (int i = 0; i < rocks.size(); i++)
         {
-            if (rocks[i] - LastRock < mid)
-            {
-                rock++;
-            }          
-            else
+            if (rocks[i] - LastRock >= mid)
             {
                 LastRock = rocks[i];
+            }
+            else
+            {
+                RemoveCount++;
             }
         }
         if (distance - LastRock < mid)
         {
-            rock++;
+            RemoveCount++;
         }
         
-        if (rock <= n)
+        if (RemoveCount <= n)
         {
-            answer = max(answer, mid);
-            start = mid + 1;
+            answer = mid;
+            Start = mid + 1;
         }
         else
         {
-            end = mid - 1;
+            End = mid - 1;
         }
+        
     }
     return answer;
 }
