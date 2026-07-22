9503f25ceb48b80265c3a6f6af3c5ac3daea2cc6
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-22 21:20:56 +0900

    [level 3] Title: 순위, Time: 0.86 ms, Memory: 4.82 MB -BaekjoonHub

--- C++/프로그래머스/3/49191. 순위/순위.cpp
+++ C++/프로그래머스/3/49191. 순위/순위.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-int solution(int n, vector<vector<int>> results) {    
+int solution(int n, vector<vector<int>> results) {
     vector<vector<int>> Table(n + 1, vector<int>(n + 1));
-    for(const vector<int>& result : results)
+    for (const vector<int> res : results)
     {
-        Table[result[0]][result[1]] = 1;
-        Table[result[1]][result[0]] = -1;
+        Table[res[0]][res[1]] = 1;
+        Table[res[1]][res[0]] = -1;
     }
     
     for (int k = 1; k <= n; k++)
@@ -17,35 +17,31 @@ int solution(int n, vector<vector<int>> results) {
         {
             for (int j = 1; j <= n; j++)
             {
-                if (Table[i][k] == 1 && Table[k][j] == 1)
+                if (Table[i][k] && Table[i][k] == Table[k][j])
                 {
-                    Table[i][j] = 1;
-                    Table[j][i] = -1;
-                }
-                else if (Table[i][k] == -1 && Table[k][j] == -1)
-                {
-                    Table[i][j] = -1;
-                    Table[j][i] = 1;
+                    Table[i][j] = Table[i][k];
+                    Table[j][i] = -Table[i][k];
                 }
             }
         }
     }
-    
-    int answer = 0, WinLose;
+        
+    int answer = 0;
     for (int i = 1; i <= n; i++)
     {
-        WinLose = 0;
+        int res = 0;
         for (int j = 1; j <= n; j++)
         {
             if (Table[i][j])
             {
-                WinLose++;
+                res++;
             }
         }
-        if (WinLose == n - 1)
+        if (res == n - 1)
         {
             answer++;
         }
     }
+
     return answer;
 }
