92b3f25374e64b327892ec4c3f6f489bd087f43e
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-22 16:14:46 +0900

    [level 3] Title: N으로 표현, Time: 2.43 ms, Memory: 4.64 MB -BaekjoonHub

--- C++/프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ C++/프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -5,8 +5,13 @@
 using namespace std;
 
 int solution(int N, int number) {
+    if (N == number)
+    {
+        return 1;
+    }
+    
     vector<unordered_set<int>> DP(9);
-    for (int i = 1; i <= 8; i++)
+    for (int i = 1; i < 9; i++)
     {
         int Num = stoi(string(i, '0' + N));
         if (Num == number)
@@ -16,22 +21,15 @@ int solution(int N, int number) {
         DP[i].insert(Num);
     }
     
-    for (int i = 2; i <= 8; i++)
+    for (int i = 2; i < 9; i++)
     {
         for (int j = 1; j < i; j++)
         {
             for (int op1 : DP[j])
             {
-                for (int op2 : DP[i -j])
+                for (int op2 : DP[i - j])
                 {
                     int Num = op1 + op2;
-                    if (Num == number)
-                    {
-                        return i;
-                    }
-                    DP[i].insert(Num);
-                    
-                    Num = op1 * op2;
                     if (Num == number)
                     {
                         return i;
@@ -45,7 +43,14 @@ int solution(int N, int number) {
                     }
                     DP[i].insert(Num);
                     
-                    if (op2 != 0)
+                    Num = op1 * op2;
+                    if (Num == number)
+                    {
+                        return i;
+                    }
+                    DP[i].insert(Num);
+                    
+                    if (op2)
                     {
                         Num = op1 / op2;
                         if (Num == number)
