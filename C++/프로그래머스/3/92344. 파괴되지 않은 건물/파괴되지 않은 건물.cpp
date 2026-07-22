21f855c278bf2c0d61c92231a6af941a0fea1078
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-23 00:12:55 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 18.90 ms, Memory: 115 MB -BaekjoonHub

--- C++/프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
+++ C++/프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
@@ -4,57 +4,76 @@
 using namespace std;
 
 int solution(vector<vector<int>> board, vector<vector<int>> skill) {
-    int N = board.size(), M = board[0].size();
-    vector<vector<int>> Table(N, vector<int>(M));
-    for (const vector<int>& s : skill)
+    vector<vector<int>> Table(board.size(), vector<int>(board[0].size()));
+    for (const vector<int>& sk : skill)
     {
-        int type = s[0], r1 = s[1], c1 = s[2], r2 = s[3], c2 = s[4], degree = s[5];
+        int type = sk[0], r1 = sk[1], c1 = sk[2], r2 = sk[3], c2 = sk[4], degree = sk[5];
         if (type == 1)
         {
-            degree *= -1;
+            Table[r1][c1] -= degree;
+            if (r2 + 1 < board.size())
+            {
+                Table[r2 + 1][c1] += degree;
+            }
+            if (c2 + 1 < board[0].size())
+            {
+                Table[r1][c2 + 1] += degree;
+            }
+            if (r2 + 1 < board.size() && c2 + 1 < board[0].size())
+            {
+                Table[r2 + 1][c2 + 1] -= degree;
+            }
         }
-        
-        Table[r1][c1] += degree;
-        if (c2 + 1 < M)
+        else
         {
-            Table[r1][c2 + 1] -= degree;
-        }
-        if (r2 + 1 < N)
-        {
-            Table[r2 + 1][c1] -= degree;
-        }
-        if (c2 + 1 < M && r2 + 1 < N)
-        {
-            Table[r2 + 1][c2 + 1] += degree;
+            Table[r1][c1] += degree;
+            if (r2 + 1 < board.size())
+            {
+                Table[r2 + 1][c1] -= degree;
+            }
+            if (c2 + 1 < board[0].size())
+            {
+                Table[r1][c2 + 1] -= degree;
+            }
+            if (r2 + 1 < board.size() && c2 + 1 < board[0].size())
+            {
+                Table[r2 + 1][c2 + 1] += degree;
+            }
         }
     }
     
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < board.size(); i++)
     {
-        for (int j = 1; j < M; j++)
+        for (int j = 1; j < board[i].size(); j++)
         {
             Table[i][j] += Table[i][j - 1];
         }
     }
-    for (int j = 0; j < M; j++)
+    for (int j = 0; j < board[0].size(); j++)
     {
-        for (int i = 1; i < N; i++)
+        for (int i = 1; i < board.size(); i++)
         {
             Table[i][j] += Table[i - 1][j];
         }
     }
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 0; j < board.size(); j++)
+        {
+            board[i][j] += Table[i][j];
+        }
+    }
     
     int answer = board.size() * board[0].size();
-    for (int i = 0; i < N; i++)
+    for (const vector<int>& row : board)
     {
-        for (int j = 0; j < M; j++)
+        for (int n : row)
         {
-            if (board[i][j] + Table[i][j] <= 0)
+            if (n <= 0)
             {
                 answer--;
             }
         }
     }
-    
     return answer;
 }
