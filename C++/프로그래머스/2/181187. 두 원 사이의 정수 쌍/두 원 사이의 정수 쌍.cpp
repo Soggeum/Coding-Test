f01311dae9efa931b7a4346f0ec117f84efa5275
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-22 21:52:42 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 7.88 ms, Memory: 4.95 MB -BaekjoonHub

--- C++/프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cpp
+++ C++/프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cpp
@@ -4,27 +4,19 @@
 
 using namespace std;
 
-long long Caculate(double a, double b) {
-    if (b == 0)
-        return floor(a) * 2LL + 1;
-    
-    long long A = floor(a);  // 명시적 floor
-    long long B = ceil(b);   // 명시적 ceil
-    
-    return (A - B + 1) * 2LL;
-}
-
 long long solution(int r1, int r2) {
     long long answer = 0;
-    for (int i = -r2; i <= r2; ++i) {
-        long long ii = 1LL * i * i;
-        long long r1sq = 1LL * r1 * r1;
-        long long r2sq = 1LL * r2 * r2;
-        
-        if (ii > r1sq) {
-            answer += Caculate(sqrt(r2sq - ii), 0);
-        } else {
-            answer += Caculate(sqrt(r2sq - ii), sqrt(r1sq - ii));
+    for (int x = -r2; x <= r2; x++)
+    {
+        if (x <= -r1 || x >= r1)
+        {
+            answer += floor(sqrt(1ll * r2 * r2 - 1ll * x * x)) * 2 + 1;
+        }
+        else
+        {
+            int y2 = floor(sqrt(1ll * r2 * r2 - 1ll * x * x));
+            int y1 = ceil(sqrt(1ll * r1 * r1 - 1ll * x * x));
+            answer += (y2 - y1 + 1) * 2;
         }
     }
     return answer;
