90606515c7f883a6245cf58fbef9b322e4c69b26
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-25 00:32:59 +0900

    [level 3] Title: 광고 삽입, Time: 45.08 ms, Memory: 53.4 MB -BaekjoonHub

--- C++/프로그래머스/3/72414. 광고 삽입/광고 삽입.cpp
+++ C++/프로그래머스/3/72414. 광고 삽입/광고 삽입.cpp
@@ -3,78 +3,87 @@
 
 using namespace std;
 
-int TimeToSec(const string& Time)
+int GetSec(const string& Time)
 {
-    int result = 0;
-    result += stoi(Time.substr(0, 2)) * 60 * 60;
-    result += stoi(Time.substr(3, 2)) * 60;
-    result += stoi(Time.substr(6, 2));
-    
-    return result;
+    int Res = 0;
+    Res += stoi(Time.substr(0, 2)) * 60 * 60;
+    Res += stoi(Time.substr(3, 2)) * 60;
+    Res += stoi(Time.substr(6, 2));
+    return Res;
 }
 
-string SecToTime(int Sec)
+string GetTime(int Sec)
 {
-    string result;
     int Hour = Sec / (60 * 60);
-    Sec %= 60 * 60;
-    int Minute = Sec / 60;
-    Sec %= 60;
+    Sec -= Hour * 60 * 60;
+    int Min = Sec / 60;
+    Sec -= Min * 60;
+    
+    string Res;
     if (Hour < 10)
     {
-        result.push_back('0');
-    }    
-    result.append(to_string(Hour));
-    result.push_back(':');
-    if (Minute < 10)
-    {
-        result.push_back('0');
+        Res.push_back('0');
     }
-    result.append(to_string(Minute));
-    result.push_back(':');
+    Res.append(to_string(Hour));
+    Res.push_back(':');
+    if (Min < 10)
+    {
+        Res.push_back('0');
+    }
+    Res.append(to_string(Min));
+    Res.push_back(':');
     if (Sec < 10)
     {
-        result.push_back('0');
+        Res.push_back('0');
     }
-    result.append(to_string(Sec));
-    return result;
+    Res.append(to_string(Sec));
+    
+    return Res;
 }
 
 string solution(string play_time, string adv_time, vector<string> logs) {
-    int TotalTime = TimeToSec("99:59:59");
-    vector<int> TimeTable(TotalTime);
+    vector<int> Table(GetSec(play_time));
     for (const string& log : logs)
     {
-        string Start = log.substr(0, 8);
-        string End = log.substr(9);
-        TimeTable[TimeToSec(Start)]++;
-        TimeTable[TimeToSec(End)]--;
-    }
-    
-    for (int i = 1; i < TimeTable.size(); i++)
-    {
-        TimeTable[i] += TimeTable[i - 1];
-    }
-    
-    int k = TimeToSec(adv_time);
-    long long PlayCount = 0;
-    int Time = 0;
-    for (; Time < k; Time++)
-    {
-        PlayCount += TimeTable[Time];
-    }
-    
-    long long MaxCount = PlayCount, InitTime = 0;
-    for (; Time < TotalTime; Time++)
-    {
-        PlayCount -= TimeTable[Time - k];
-        PlayCount += TimeTable[Time];
-        if (PlayCount > MaxCount)
+        string Start = log.substr(0, 8), End = log.substr(9);
+        int start_sec = GetSec(Start);
+        int end_sec = GetSec(End);
+
+        Table[start_sec] += 1;
+        if (end_sec < Table.size())
         {
-            MaxCount = PlayCount;
-            InitTime = Time - k + 1;
+            Table[end_sec] -= 1; 
         }
     }
     
-    return SecToTime(InitTime);
+    for (int i = 1; i < Table.size(); i++)
+    {
+        Table[i] += Table[i - 1];
+    }
+    
+    int Start = 0, End = GetSec(adv_time);
+    long long Sum = 0, MaxSum = 0;
+    for (int i = Start; i < End; i++)
+    {
+        Sum += Table[i];
+    }
+    
+    string answer = "00:00:00";
+    while (End <= Table.size())
+    {
+        if (Sum > MaxSum)
+        {
+            answer = GetTime(Start);
+            MaxSum = Sum;
+        }
+        
+        Sum -= Table[Start++];
+        if (End == Table.size())
+        {
+            break;
+        }
+        Sum += Table[End++];
+    }
+    
+    return answer;
 }
