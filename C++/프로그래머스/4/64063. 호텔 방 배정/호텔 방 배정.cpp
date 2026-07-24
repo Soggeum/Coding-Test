7b0e19d1e121593c3aab6caa1ad75d6b7026fac3
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-25 00:53:37 +0900

    [level 4] Title: 호텔 방 배정, Time: 121.76 ms, Memory: 52.8 MB -BaekjoonHub

--- C++/프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.cpp
+++ C++/프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.cpp
@@ -1,47 +1,30 @@
-// Union-Find와 경로 압축 (lazy Optimization)
 #include <string>
 #include <vector>
 #include <unordered_map>
 
 using namespace std;
 
-long long GetNextRoom(long long Request, unordered_map<long long, long long>& NextRoomMap)
+long long GetRoom(long long rn, unordered_map<long long, long long>& um)
 {
-    vector<long long> Visited;
-    Visited.push_back(Request);
-    
-    auto it = NextRoomMap.find(Request);
-    while(it != NextRoomMap.end())
+    auto it = um.find(rn);
+    if (it == um.end())
     {
-        Visited.push_back(Request);
-        Request = it->second;
-        it = NextRoomMap.find(Request);
-    }
-    Visited.push_back(Request);
-    
-    for (long long RoomNumber : Visited)
-    {
-        NextRoomMap[RoomNumber] = Request + 1;
+        um[rn] = rn + 1;
+        return rn;
     }
     
-    return Request;
+    it->second = GetRoom(it->second, um);
+    return it->second;
 }
 
 vector<long long> solution(long long k, vector<long long> room_number) {
+    unordered_map<long long, long long> um;
     vector<long long> answer;
-    unordered_map<long long, long long> NextRoomMap;
-    for (long long Request : room_number)
+    
+    for (long long rn : room_number)
     {
-        if (NextRoomMap.count(Request))
-        {
-            long long NextRoom = GetNextRoom(Request, NextRoomMap);
-            answer.push_back(NextRoom);
-        }
-        else
-        {
-            answer.push_back(Request);
-            NextRoomMap[Request] = Request + 1;
-        }
+        long long Room = GetRoom(rn, um);
+        answer.push_back(Room);
     }
     
     return answer;
