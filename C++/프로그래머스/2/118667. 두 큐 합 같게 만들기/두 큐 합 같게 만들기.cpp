c7678f69853e2af90e8dc7a366b558ca05154c13
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-28 02:01:59 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 3.58 ms, Memory: 27.3 MB -BaekjoonHub

--- C++/프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
+++ C++/프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -4,56 +4,63 @@
 using namespace std;
 
 int solution(vector<int> queue1, vector<int> queue2) {
+    long long TotalSum = 0;
+    for (int n : queue1)
+    {
+        TotalSum += n;
+    }
+    for (int n : queue2)
+    {
+        TotalSum += n;
+    }
+    if (TotalSum & 1)
+    {
+        return -1;
+    }
+    TotalSum >>= 1;
+    
     vector<int> q;
-    long long Sum = 0;
+    q.reserve(queue1.size() * 3);
     for (int n : queue1)
     {
         q.push_back(n);
-        Sum += n;
-        
     }
     for (int n : queue2)
     {
         q.push_back(n);
-        Sum += n;
     }
     for (int n : queue1)
     {
         q.push_back(n);
     }
     
-    if (Sum & 1)
-    {
-        return -1;
-    }
-    Sum = Sum >> 1;
-    
     int answer = 0, Start = 0, End = queue1.size();
-    long long CurrSum = 0;
+    long long Sum = 0;
     for (int i = Start; i < End; i++)
     {
-        CurrSum += q[i];
+        Sum += queue1[i];
     }
-    while (Start <= End && End <= q.size())
+    while (End <= q.size())
     {
-        if (CurrSum > Sum)
+        if (Sum == TotalSum)
         {
-            CurrSum -= q[Start++];
-            answer++;
+            return answer;
         }
-        else if (CurrSum < Sum)
+        else if (Sum < TotalSum)
         {
             if (End == q.size())
             {
                 return -1;
             }
-            CurrSum += q[End++];
+            Sum += q[End++];
             answer++;
         }
         else
         {
-            return answer;
+            Sum -= q[Start++];
+            answer++;
         }
     }
+    
     return -1;
 }
