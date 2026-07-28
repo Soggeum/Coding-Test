9b21575eb0b5f70eb998634b2868d6be8e7b67ba
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-28 13:46:53 +0900

    [level 3] Title: 스티커 모으기(2), Time: 0.58 ms, Memory: 7.34 MB -BaekjoonHub

--- C++/프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
+++ C++/프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
@@ -4,30 +4,26 @@ using namespace std;
 
 int solution(vector<int> sticker)
 {
-    if (sticker.size() == 1)
+    if (sticker.size() < 4)
     {
-        return sticker[0];
+        int answer = 0;
+        for (int n : sticker)
+        {
+            answer = max(answer, n);
+        }
+        return answer;
     }
-    if (sticker.size() == 2)
-    {
-        return max(sticker[0], sticker[1]);
-    }
-    
-    vector<int> DP1(sticker.size() - 1);
+    vector<int> DP1(sticker.size()), DP2(sticker.size());
     DP1[0] = sticker[0];
     DP1[1] = sticker[0];
-    for (int i = 2; i < sticker.size() - 1; i++)
-    {
-        DP1[i] = max(DP1[i - 1], DP1[i - 2] + sticker[i]);
-    }
-
-    vector<int> DP2(sticker.size());
     DP2[0] = 0;
     DP2[1] = sticker[1];
     for (int i = 2; i < sticker.size(); i++)
     {
+        DP1[i] = max(DP1[i - 1], DP1[i - 2] + sticker[i]);
         DP2[i] = max(DP2[i - 1], DP2[i - 2] + sticker[i]);
     }
-
+    DP1[sticker.size() - 1] = DP1[sticker.size() - 2];
+    
     return max(DP1.back(), DP2.back());
 }
