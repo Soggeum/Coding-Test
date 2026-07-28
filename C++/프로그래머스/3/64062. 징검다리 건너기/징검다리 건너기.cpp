60a57280fc2ec69454a65ef9336914d9d4e57d55
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-28 23:58:21 +0900

    [level 3] Title: 징검다리 건너기, Time: 63.67 ms, Memory: 15 MB -BaekjoonHub

--- C++/프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
+++ C++/프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -5,22 +5,25 @@
 using namespace std;
 
 int solution(vector<int> stones, int k) {
-    int Start = 0, End = k, answer = 200000000;
     multiset<int> ms;
+    int Start = 0, End = k;
     for (int i = Start; i < End; i++)
     {
         ms.insert(stones[i]);
     }
+    
+    int answer = *ms.rbegin();
     while (End <= stones.size())
     {
-        answer = min(answer, *ms.rbegin());
+        const auto& it = ms.find(stones[Start++]);
+        ms.erase(it);
         if (End == stones.size())
         {
             break;
         }
         ms.insert(stones[End++]);
-        auto it = ms.find(stones[Start++]);
-        ms.erase(it);
+        
+        answer = min(answer, *ms.rbegin());
     }
     return answer;
 }
