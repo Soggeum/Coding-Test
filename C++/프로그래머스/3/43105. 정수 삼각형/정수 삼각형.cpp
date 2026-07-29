7ecc182a602dd1d22f860e0da01fa3d3889fe0b6
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-29 15:06:26 +0900

    [level 3] Title: 정수 삼각형, Time: 0.26 ms, Memory: 8.09 MB -BaekjoonHub

--- C++/프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ C++/프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -10,7 +10,7 @@ int solution(vector<vector<int>> triangle) {
         {
             if (j == 0)
             {
-                triangle[i][j] += triangle[i - 1][j];
+                triangle[i][j] += triangle[i - 1][j];                
             }
             else if (j == triangle[i].size() - 1)
             {
@@ -18,10 +18,10 @@ int solution(vector<vector<int>> triangle) {
             }
             else
             {
-                triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j]);
-            }
+                triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j]);                    }
         }
     }
+    
     int answer = 0;
     for (int n : triangle.back())
     {
