03c5c3b77173b143a56787c04e0b976dcd602e56
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-31 00:34:46 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 0.04 ms, Memory: 4.95 MB -BaekjoonHub

--- C++/프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.cpp
+++ C++/프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.cpp
@@ -4,44 +4,39 @@
 
 using namespace std;
 
-vector<long long> PowFive(21, 5);
-vector<long long> PowFour(21, 4);
-
-long long OneCount(int n, long long i)
+long long GetOne(int n, long long idx)
 {
+    if (idx < 0)
+    {
+        return 0;
+    }
     if (n == 1)
     {
-        return i < 3 ? i : i - 1;
+        return (idx < 2) ? idx + 1 : idx;
     }
-    //int r = (i - 1) / static_cast<long long>(pow(5, n - 1));
-    int r = (i - 1) / PowFive[n - 1];
     
-    long long res = 0;
-    if (r < 2)
+    long long Bundle = pow(5, n - 1);
+    int BundleIdx = idx / Bundle;
+    if (BundleIdx < 2)
     {
-        //res += static_cast<long long>(pow(4, n - 1)) * r;
-        res += PowFour[n - 1] * r;
+        long long res = 0;
+        res += pow(4, n - 1) * BundleIdx;
+        return res + GetOne(n - 1, idx % static_cast<long long>(pow(5, n - 1)));
     }
-    else if (r == 2)
+    else if (BundleIdx == 2)
     {
-        return PowFour[n - 1] * 2;
+        return pow(4, n - 1) * 2;
     }
     else
     {
-        //res += static_cast<long long>(pow(4, n - 1)) * (r - 1);
-        res += PowFour[n - 1] * (r - 1);
+        long long res = 0;
+        res += pow(4, n - 1) * (BundleIdx - 1);
+        return res + GetOne(n - 1, idx % static_cast<long long>(pow(5, n - 1)));
     }
     
-    //i -= static_cast<long long>(pow(5, n - 1)) * r;
-    i -= PowFive[n - 1] * r;
-    return res + OneCount(n - 1, i);
 }
 
 int solution(int n, long long l, long long r) {
-    for (int i = 2; i < 21; i++)
-    {
-        PowFive[i] = PowFive[i - 1] * 5;
-        PowFour[i] = PowFour[i - 1] * 4;
-    }
-    return OneCount(n, r) - OneCount(n, l - 1);
+    int answer = 0;
+    return GetOne(n, r - 1) - GetOne(n, l - 2);
 }
