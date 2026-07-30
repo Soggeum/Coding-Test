f5903c3a6ed19e0ca1a4a666d8152385324d96d0
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-07-31 00:39:13 +0900

    [level 4] Title: 도둑질, Time: 5.91 ms, Memory: 40.8 MB -BaekjoonHub

--- C++/프로그래머스/4/42897. 도둑질/도둑질.cpp
+++ C++/프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -6,19 +6,19 @@ using namespace std;
 int solution(vector<int> money) {
     vector<int> DP1(money.size()), DP2(money.size());
     DP1[0] = money[0];
-    DP1[1] = money[0];
+    DP1[1] = DP1[0];
     for (int i = 2; i < money.size() - 1; i++)
     {
-        DP1[i] = max(DP1[i - 1], DP1[i - 2] + money[i]);
+        DP1[i] = max(DP1[i - 2] + money[i], DP1[i - 1]);
     }
-    DP1[money.size() - 1] = DP1[money.size() - 2];
+    int answer = DP1[money.size() - 2];
     
     DP2[0] = 0;
     DP2[1] = money[1];
-    for (int i = 2; i < money.size(); i++)
+    for (int i = 2; i< money.size(); i++)
     {
-        DP2[i] = max(DP2[i - 1], DP2[i - 2] + money[i]);
+        DP2[i] = max(DP2[i - 2] + money[i], DP2[i - 1]);
     }
-    
-    return max(DP1.back(), DP2.back());
+    answer = max(answer, DP2[money.size()  -1]);
+    return answer;
 }
