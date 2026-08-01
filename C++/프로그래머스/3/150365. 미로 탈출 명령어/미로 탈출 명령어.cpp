592b665c93c47ac6098392bda00e77a33b751871
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-08-02 01:54:47 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.04 ms, Memory: 4.95 MB -BaekjoonHub

--- C++/프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
+++ C++/프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
@@ -1,51 +1,42 @@
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-bool IsPossible(int n, int m, int x, int y, int r, int c, int k)
-{
-    if (x < 1 || x > n || y < 1 || y > m)
-    {
-        return false;
-    }
-    
-    int Move = abs(x - r) + abs(y - c);
-    if (Move > k)
-    {
-        return false;
-    }
-    return !((Move - k) & 1);
-}
-
 string solution(int n, int m, int x, int y, int r, int c, int k) {
-    string answer = "";
-    
-    int Move = abs(x - r) + abs(y - c);
-    if (Move > k || (Move - k) & 1)
+    vector<char> dir = {'d', 'l', 'r', 'u'};
+    vector<int> dx = {1, 0, 0, -1};
+    vector<int> dy = {0, -1, 1, 0};
+
+    int MoveCount = abs(x - r) + abs(y - c);
+    if (MoveCount > k || (MoveCount - k) & 1)
     {
         return "impossible";
     }
     
-    vector<int> dy = {0, -1, 1, 0};
-    vector<int> dx = {1, 0, 0, -1};
-    vector<char> dir = {'d', 'l', 'r', 'u'};
-    
-    while (k)
+    string answer = "";
+    while(k)
     {
-        k--;
-        for (int i = 0; i < dx.size(); i++)
+        for (int i = 0; i < dir.size(); i++)
         {
             int NewX = x + dx[i], NewY = y + dy[i];
-            if (IsPossible(n, m, NewX, NewY, r, c, k))
+            if (NewX < 1 || NewX > n || NewY < 1 || NewY > m)
             {
-                x = NewX;
-                y = NewY;
-                answer.push_back(dir[i]);
-                break;
+                continue;
             }
+            if (abs(NewX - r) + abs(NewY - c) > k)
+            {
+                continue;
+            }
+            
+            x = NewX;
+            y = NewY;
+            answer.push_back(dir[i]);
+            break;
         }
+        
+        k--;
     }
-    
     return answer;
 }
