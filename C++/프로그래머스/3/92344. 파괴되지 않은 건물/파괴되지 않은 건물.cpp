4fac9477f05b3cd831a60fbe51c61e573ae570a4
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-08-02 23:53:31 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 16.34 ms, Memory: 115 MB -BaekjoonHub

--- C++/프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
+++ C++/프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
@@ -16,27 +16,30 @@ int solution(vector<vector<int>> board, vector<vector<int>> skill) {
         Table[r1][c1] += degree;
         Table[r1][c2 + 1] -= degree;
         Table[r2 + 1][c1] -= degree;
-        Table[r2 + 1][c2 + 1] += degree;
+        Table[r2 + 1][c2 + 1] += degree;        
     }
     
-    for (int i = 0;i < board.size(); i++){
-        for (int j = 1;j < board[i].size(); j++)
-        {
-            Table[i][j] += Table[i][j - 1];
-        }
-    }
-    for (int i = 1;i < board.size(); i++){
-        for (int j = 0;j < board[i].size(); j++)
+    for (int i = 1; i < board.size(); i++)
+    {
+        for (int j = 0; j < board[i].size(); j++)
         {
             Table[i][j] += Table[i - 1][j];
         }
     }
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 1; j < board[i].size(); j++)
+        {
+            Table[i][j] += Table[i][j - 1];
+        }
+    }
     
     int answer = 0;
-    for (int i = 0;i < board.size(); i++){
-        for (int j = 0;j < board[i].size(); j++)
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 0; j < board[i].size(); j++)
         {
-            if (Table[i][j] + board[i][j] > 0)
+            if (board[i][j] + Table[i][j] > 0)
             {
                 answer++;
             }
