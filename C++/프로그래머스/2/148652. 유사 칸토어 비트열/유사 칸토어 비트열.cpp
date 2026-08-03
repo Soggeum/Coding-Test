c52e7f6a33f0b6c6ece59844cc26fa5893596263
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-08-03 23:20:33 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 0.02 ms, Memory: 4.95 MB -BaekjoonHub

--- C++/프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.cpp
+++ C++/프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.cpp
@@ -4,39 +4,28 @@
 
 using namespace std;
 
-long long GetOne(int n, long long idx)
+long long GetOne(int n, long long i)
 {
-    if (idx < 0)
+    if (i < 0)
     {
         return 0;
     }
     if (n == 1)
     {
-        return (idx < 2) ? idx + 1 : idx;
+        return i < 2 ? i + 1 : i;
     }
     
-    long long Bundle = pow(5, n - 1);
-    int BundleIdx = idx / Bundle;
-    if (BundleIdx < 2)
-    {
-        long long res = 0;
-        res += pow(4, n - 1) * BundleIdx;
-        return res + GetOne(n - 1, idx % static_cast<long long>(pow(5, n - 1)));
-    }
-    else if (BundleIdx == 2)
-    {
-        return pow(4, n - 1) * 2;
-    }
-    else
-    {
-        long long res = 0;
-        res += pow(4, n - 1) * (BundleIdx - 1);
-        return res + GetOne(n - 1, idx % static_cast<long long>(pow(5, n - 1)));
-    }
+    long long res = 0;
+    int Idx = i / static_cast<long long>(pow(5, n - 1));
+    res += (Idx < 3 ? pow(4, n - 1) * Idx : pow(4, n - 1) * (Idx - 1)); 
     
+    if (Idx == 2)
+    {
+        return res;
+    }
+    return res + GetOne(n - 1, i % static_cast<long long>(pow(5, n - 1)));
 }
 
 int solution(int n, long long l, long long r) {
-    int answer = 0;
     return GetOne(n, r - 1) - GetOne(n, l - 2);
 }
