5952e7212cc3e516457a2fc4378458734cfb7a64
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-08-03 18:38:40 +0900

    [level 3] Title: 거스름돈, Time: 1.15 ms, Memory: 4.73 MB -BaekjoonHub

--- C++/프로그래머스/3/12907. 거스름돈/거스름돈.cpp
+++ C++/프로그래머스/3/12907. 거스름돈/거스름돈.cpp
@@ -10,9 +10,8 @@ int solution(int n, vector<int> money) {
     {
         for (int i = m; i <= n; i++)
         {
-            DP[i] = (DP[i] + DP[i - m]) % 1000000007;
+            DP[i] += DP[i - m];
         }
     }
-    
     return DP[n];
 }
