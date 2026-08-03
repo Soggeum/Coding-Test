85ca71320529286bae72bc3a18b4bacc171d1760
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-08-03 14:28:55 +0900

    [level 3] Title: 스타 수열, Time: 5.44 ms, Memory: 24.8 MB -BaekjoonHub

--- C++/프로그래머스/3/70130. 스타 수열/스타 수열.cpp
+++ C++/프로그래머스/3/70130. 스타 수열/스타 수열.cpp
@@ -1,69 +1,31 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-struct Node
-{
-    int CrossNum, Count;    
-    bool operator<(const Node& Other) const
-    {
-        return Count < Other.Count;
-    }
-};
-
 int solution(std::vector<int> a) {
-    vector<int> Table(a.size());
-    for (int n : a)
-    {
-        Table[n]++;
-    }
+    vector<int> LastIdx(a.size() , -1);
+    vector<int> Num(a.size());
     
-    priority_queue<Node> pq;
-    for (int i = 0; i < Table.size(); i++)
+    for (int i = 0; i < a.size(); i++)
     {
-        pq.push({i, Table[i]});
+        int num = a[i];
+        if (i - 1 != LastIdx[num] && a[i - 1] != num)
+        {
+            Num[num] += 2;
+            LastIdx[num] = i;
+        }
+        else if (i + 1 < a.size() && a[i + 1] != num)
+        {
+            Num[num] += 2;
+            LastIdx[num] = i + 1;
+        }
     }
     
     int answer = 0;
-    while(!pq.empty())
+    for (int n : Num)
     {
-        int CrossNum = pq.top().CrossNum, Count = pq.top().Count;
-        pq.pop();
-        
-        if (Count * 2 <= answer)
-        {
-            continue;
-        }
-        
-        int LastIdx = -1, Len = 0;
-        for (int i = 0; i < a.size(); i++)
-        {
-            if (a[i] == CrossNum)
-            {
-                Count--;
-                if (i - 1 >= 0 && a[i - 1] != CrossNum && i - 1 > LastIdx)
-                {
-                    Len += 2;
-                    LastIdx = i;
-                }
-                else if (i + 1 < a.size() && a[i + 1] != CrossNum)
-                {
-                    Len += 2;
-                    LastIdx = i + 1;
-                    i++;
-                }
-                
-                if (Count == 0)
-                {
-                    break;
-                }
-            }
-        }
-        
-        answer = max(answer, Len);
+        answer = max(answer, n);
     }
-    
     return answer;
 }
