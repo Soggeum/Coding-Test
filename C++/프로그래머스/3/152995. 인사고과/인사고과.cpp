7725f47c8320e192094c61b75f93fbab9ca8838b
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-08-03 19:36:03 +0900

    [level 3] Title: 인사고과, Time: 16.57 ms, Memory: 21.8 MB -BaekjoonHub

--- C++/프로그래머스/3/152995. 인사고과/인사고과.cpp
+++ C++/프로그래머스/3/152995. 인사고과/인사고과.cpp
@@ -6,39 +6,35 @@ using namespace std;
 
 bool comp(const vector<int>& a, const vector<int>& b)
 {
-    if (a[0] == b[0])
+    if (a[0] != b[0])
     {
-        return a[1] < b[1];
+        return a[0] > b[0];
     }
-    return a[0] > b[0];
+    return a[1] < b[1];
 }
 
 int solution(vector<vector<int>> scores) {
     vector<int> wanho = scores[0];
     sort(scores.begin(), scores.end(), comp);
-    int answer = 1, Max = 0, wscore = wanho[0] + wanho[1];
+    int MaxPeer = -1, answer = 1;
     for (const vector<int>& s : scores)
     {
         if (s == wanho)
         {
-            if (wanho[1] < Max)
+            if (s[1] < MaxPeer)
             {
                 return -1;
             }
         }
-        else
+        if (s[1] >= MaxPeer)
         {
-            if (s[1] >= Max)
+            MaxPeer = s[1];
+            if (s[0] + s[1] > wanho[0] + wanho[1])
             {
-                Max = s[1];
-                if (s[0] + s[1] > wscore)
-                {
-                    answer++;
-                }
+                answer++;
             }
         }
     }
-    
     
     return answer;
 }
