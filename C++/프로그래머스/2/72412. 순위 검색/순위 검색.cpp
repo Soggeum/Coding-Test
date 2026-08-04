f0c678f5c84e55b798f3cd279fee22999c85bfd7
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-08-05 00:34:56 +0900

    [level 2] Title: 순위 검색, Time: 134.60 ms, Memory: 43.5 MB -BaekjoonHub

--- C++/프로그래머스/2/72412. 순위 검색/순위 검색.cpp
+++ C++/프로그래머스/2/72412. 순위 검색/순위 검색.cpp
@@ -1,94 +1,70 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <sstream>
 #include <algorithm>
 
 using namespace std;
 
-void DFS(const vector<vector<string>>& Comb, vector<string>& Cur, int i, map<vector<string>, vector<int>>& m)
+void Put(unordered_map<string, vector<int>>& Table, const vector<string>& A, int X)
 {
-    if (i == 4)
+    for (int i = 0; i < (1 << 4); i++)
     {
-        m[Cur] = {};
-        return;
-    }
-    
-    for (int j = 0; j < Comb[i].size(); j++)
-    {
-        Cur.push_back(Comb[i][j]);
-        DFS(Comb, Cur, i + 1, m);
-        Cur.pop_back();
-    }
-}
-
-void Push(const vector<vector<string>>& Comb, vector<string>& Cur, int i, map<vector<string>, vector<int>>& m, int X)
-{
-    if (i == 4)
-    {
-        m[Cur].push_back(X);
-        return;
-    }
-    
-    for (int j = 0; j < Comb[i].size(); j++)
-    {
-        Cur.push_back(Comb[i][j]);
-        Push(Comb, Cur, i + 1, m, X);
-        Cur.pop_back();
+        string key;
+        for (int j = 0; j < 4; j++)
+        {
+            if (i & (1 << j))
+            {
+                key.append(A[j]);
+            }
+            else
+            {
+                key.push_back('-');
+            }
+            if (j < 3)
+            {
+                key.append(" and ");
+            }
+        }
+        
+        Table[key].push_back(X);
     }
 }
 
 vector<int> solution(vector<string> info, vector<string> query) {
-    vector<vector<string>> Comb = 
-    {
-        {"cpp", "java", "python", "-"},
-        {"backend", "frontend", "-"},
-        {"junior", "senior", "-"},
-        {"chicken", "pizza", "-"}
-    };
-    map<vector<string>, vector<int>> m;
-    vector<string> Cur;
-    DFS(Comb, Cur, 0, m);
-    
-    vector<vector<string>> Base = 
-        {
-        {"-"},
-        {"-"},
-        {"-"},
-        {"-"}
-    };
+    unordered_map<string, vector<int>> Table;
     for (const string& i : info)
     {
-        vector<string> is(4);
+        vector<string> A(4);
         int X;
         stringstream ss(i);
-        ss >> is[0] >> is[1] >> is[2] >> is[3] >> X;
-        for (int i = 0; i < 4; i++)
-        {
-            Base[i].push_back(is[i]);
-        }
-        Push(Base, Cur, 0, m, X);
-        for (int i = 0; i < 4; i++)
-        {
-            Base[i].pop_back();
-        }
+        ss >> A[0] >> A[1] >> A[2] >> A[3] >> X;
+        
+        Put(Table, A, X);
     }
     
-    for (auto& it : m)
+    for (auto& it : Table)
     {
         sort(it.second.begin(), it.second.end());
     }
-
+    
     vector<int> answer;
     for (const string& q : query)
     {
-        stringstream ss(q);
-        vector<string> vs(4);
-        string a1, a2, a3;
+        string key;
         int X;
-        ss >> vs[0] >> a1 >> vs[1] >> a2 >> vs[2] >> a3 >> vs[3] >> X;
-        answer.push_back(m[vs].end() - lower_bound(m[vs].begin(), m[vs].end(), X));
+        for (int i = 0; i < q.size(); i++)
+        {
+            if (isdigit(q[i]))
+            {
+                key = q.substr(0, i - 1);
+                X = stoi(q.substr(i));
+                break;
+            }
+        }
+        
+        auto it = lower_bound(Table[key].begin(), Table[key].end(), X);
+        answer.push_back(Table[key].end() - it);
     }
-    
     return answer;
 }
