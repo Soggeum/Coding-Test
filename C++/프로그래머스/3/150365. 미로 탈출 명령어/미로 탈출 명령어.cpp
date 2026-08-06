f155994cabfb0d5b1482b1d9e798eb5b48e9848f
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-08-06 18:22:15 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.03 ms, Memory: 4.95 MB -BaekjoonHub

--- C++/프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
+++ C++/프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
@@ -1,41 +1,36 @@
 #include <string>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
 string solution(int n, int m, int x, int y, int r, int c, int k) {
-    vector<char> dir = {'d', 'l', 'r', 'u'};
-    vector<int> dx = {1, 0, 0, -1};
-    vector<int> dy = {0, -1, 1, 0};
-
-    int MoveCount = abs(x - r) + abs(y - c);
-    if (MoveCount > k || (MoveCount - k) & 1)
+    int Move = abs(x - r) + abs(y - c);
+    if (Move > k || (k - Move) & 1)
     {
         return "impossible";
     }
     
+    vector<char> dir = {'d', 'l', 'r', 'u'};
+    vector<int> dx = {1, 0, 0, -1};
+    vector<int> dy = {0, -1, 1, 0};
+    
     string answer = "";
-    while(k)
+    while (k)
     {
         for (int i = 0; i < dir.size(); i++)
         {
             int NewX = x + dx[i], NewY = y + dy[i];
-            if (NewX < 1 || NewX > n || NewY < 1 || NewY > m)
+            if (NewX > 0 && NewX <= n && NewY > 0 && NewY <= m)
             {
-                continue;
+                if (abs(NewX - r) + abs(NewY - c) <= k)
+                {
+                    answer.push_back(dir[i]);
+                    x = NewX;
+                    y = NewY;
+                    break;
+                }
             }
-            if (abs(NewX - r) + abs(NewY - c) > k)
-            {
-                continue;
-            }
-            
-            x = NewX;
-            y = NewY;
-            answer.push_back(dir[i]);
-            break;
         }
-        
         k--;
     }
     return answer;
