1b86d85f1ef1b6ab0c6a57798a2b21e7761a8014
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-08-08 22:33:13 +0900

    [level 2] Title: 완전범죄, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- C++/프로그래머스/2/389480. 완전범죄/완전범죄.cpp
+++ C++/프로그래머스/2/389480. 완전범죄/완전범죄.cpp
@@ -7,7 +7,7 @@ int solution(vector<vector<int>> info, int n, int m) {
     vector<vector<int>> Table(info.size(), vector<int>(m, n));
     if (info[0][0] < n)
     {
-        Table[0][0] = info[0][0];
+        Table[0][0] = info[0][0];        
     }
     if (info[0][1] < m)
     {
@@ -16,27 +16,22 @@ int solution(vector<vector<int>> info, int n, int m) {
     
     for (int i = 1; i < info.size(); i++)
     {
+        int A = info[i][0], B = info[i][1];
         for (int j = 0; j < m; j++)
         {
-            int A = info[i][0], B = info[i][1];
-            if (j - B >= 0)
-            {
-                Table[i][j] = Table[i - 1][j - B];            
-            }
-            if (Table[i - 1][j] + A < n)
-            {
-                Table[i][j] = min(Table[i][j], Table[i - 1][j] + A);
-            }
-        }
-    }
-    int answer = n;
-    for (int x : Table.back())
-    {
-        if (x < answer)
-        {
-            answer = x;
+            int opt1 = j - B >= 0 ? Table[i - 1][j - B] : n;
+            int opt2 = min(Table[i - 1][j] + A, n);
+            Table[i][j] = min(opt1, opt2);
         }
     }
     
+    int answer = n;
+    for (int x : Table.back())
+    {
+        if (x != n)
+        {
+            answer = min(answer, x);
+        }
+    }
     return answer == n ? -1 : answer;
 }
