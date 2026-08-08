294d92f03dd8fd72c722a339eb61b141ecc26eb4
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-08-08 21:51:27 +0900

    [level 3] Title: 스타 수열, Time: 4.59 ms, Memory: 24.8 MB -BaekjoonHub

--- C++/프로그래머스/3/70130. 스타 수열/스타 수열.cpp
+++ C++/프로그래머스/3/70130. 스타 수열/스타 수열.cpp
@@ -1,35 +1,35 @@
 #include <string>
 #include <vector>
 
+// Last에 각 숫자가 스타수열에 들어갔을 때 가장 마지막으로 사용된 수의 인덱스 넣기
+// 이전 숫자가 되면 넣기, 안 되면 다음 숫자.
+
 using namespace std;
 
 int solution(std::vector<int> a) {
-    vector<int> LastUsed(a.size(), -1);
+    vector<int> Last(a.size(), -1);
     vector<int> Count(a.size());
-    
     for (int i = 0; i < a.size(); i++)
     {
-        int n = a[i];
-        if (LastUsed[n] != i - 1)
+        if (Last[a[i]] != i - 1 && a[i - 1] != a[i])
         {
-            Count[n] += 2;
-            LastUsed[n] = i;
+            Last[a[i]] = i;
+            Count[a[i]] += 2;
         }
-        else if (i + 1 < a.size() && a[i + 1] != n)
+        else if (i + 1 < a.size() && a[i + 1] != a[i])
         {
-            Count[n] += 2;
-            LastUsed[n] = i + 1;
+            Last[a[i]] = i + 1;
+            Count[a[i]] += 2;
         }
         else
         {
-            LastUsed[n] = i;
+            Last[a[i]] = i;
         }
     }
-    
     int answer = 0;
-    for (int i = 0; i < Count.size(); i++)
+    for (int n : Count)
     {
-        answer = max(answer, Count[i]);
+        answer = max(answer, n);
     }
-    return answer;
+    return answer == 2 ? 0  : answer;
 }
