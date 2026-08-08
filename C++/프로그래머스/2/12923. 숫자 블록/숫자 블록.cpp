62e3c75fce5aa0404d110a0a85ab3505965f6c64
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-08-08 15:11:07 +0900

    [level 2] Title: 숫자 블록, Time: 55.83 ms, Memory: 4.93 MB -BaekjoonHub

--- C++/프로그래머스/2/12923. 숫자 블록/숫자 블록.cpp
+++ C++/프로그래머스/2/12923. 숫자 블록/숫자 블록.cpp
@@ -3,26 +3,30 @@
 
 using namespace std;
 
+// 2부터 루트까지 하면서 몫이 천만 이하면 리턴. 아니면 나눈 숫자를 나중에 리턴
+
+int GetNum(long long n)
+{
+    int res = 1;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            if (n / i <= 10000000)
+            {
+                return n / i;
+            }
+            res = i;
+        }
+    }
+    return res;
+}
+
 vector<int> solution(long long begin, long long end) {
-    vector<int> answer(end - begin + 1, 1);
+    vector<int> answer;
     for (long long i = begin; i <= end; i++)
     {
-        for (int j = 2; j * j <= i; j++)
-        {
-            if (i % j == 0)
-            {
-                long long n = i / j;
-                if (n <= 10000000)
-                {
-                    answer[i - begin] = n;
-                    break;
-                }
-                else
-                {
-                    answer[i - begin] = j;
-                }
-            }
-        }
+        answer.push_back(GetNum(i));
     }
     if (begin == 1)
     {
