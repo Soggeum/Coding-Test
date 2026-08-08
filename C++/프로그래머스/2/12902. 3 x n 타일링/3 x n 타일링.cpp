0bd10ced5c85d134e65ec9112d53b9568c2d25f9
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-08-08 14:37:13 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.03 ms, Memory: 4.42 MB -BaekjoonHub

--- C++/프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.cpp
+++ C++/프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.cpp
@@ -10,16 +10,10 @@ int solution(int n) {
     }
     vector<int> DP(n + 1);
     DP[0] = 1;
-    for (int i = 0; i <= n; i += 2)
+    DP[2] = 3;
+    for (int i = 4; i <= n; i += 2)
     {
-        if (i + 2 <= n)
-        {
-            DP[i + 2] = (DP[i + 2] + 1ll * DP[i] * 3) % 1000000007;
-        }
-        for (int j = 4; i + j <= n; j += 2)
-        {
-            DP[i + j] = (DP[i + j] + 1ll * DP[i] * 2) % 1000000007;
-        }
+        DP[i] = (4ll * DP[i - 2] - DP[i - 4] + 1000000007) % 1000000007;
     }
     
     return DP[n];
