f7d93a39a3c84ed1897a398a813a278f1ecdb8f6
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-08-08 16:36:53 +0900

    [level 3] Title: 보행자 천국, Time: 28.11 ms, Memory: 7.74 MB -BaekjoonHub

--- C++/프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
+++ C++/프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
@@ -2,26 +2,21 @@
 
 using namespace std;
 
+int MOD = 20170805;
+
 struct Node
 {
     int H, V;    
 };
 
-int MOD = 20170805;
-
 // 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 int solution(int m, int n, vector<vector<int>> city_map) {
-    vector<vector<Node>> Table(m, vector<Node>(n));
-    Table[0][0] = {1, 0};
-    for (int j = 1; j < n; j++)
+    if (m * n == 1)
     {
-        if (city_map[0][j] == 1)
-        {
-            break;
-        }
-        Table[0][j].H = 1;
+        return 1;
     }
-    for (int i = 1; i < m; i++)
+    vector<vector<Node>> Table(m, vector<Node>(n));
+    for (int i = 0; i < m; i++)
     {
         if (city_map[i][0] == 1)
         {
@@ -29,31 +24,35 @@ int solution(int m, int n, vector<vector<int>> city_map) {
         }
         Table[i][0].V = 1;
     }
-    
-    for (int i = 1; i < m; i++)
+    for (int j = 0; j < n; j++)
     {
+        if (city_map[0][j] == 1)
+        {
+            break;
+        }
+        Table[0][j].H = 1;
+    }
+    
+    for (int i = 1;i < m; i++){
         for (int j = 1; j < n; j++)
         {
             if (city_map[i][j] == 1)
             {
                 continue;
             }
+                
             if (city_map[i - 1][j] != 2)
             {
-                Table[i][j].V += Table[i - 1][j].H;
+                Table[i][j].V = (Table[i][j].V + Table[i - 1][j].H) % MOD;    
             }
-            Table[i][j].V += Table[i - 1][j].V;
-            
+            Table[i][j].V = (Table[i][j].V + Table[i - 1][j].V) % MOD;
             if (city_map[i][j - 1] != 2)
             {
-                Table[i][j].H += Table[i][j - 1].V;
+                Table[i][j].H = (Table[i][j].H + Table[i][j - 1].V) % MOD;    
             }
-            Table[i][j].H += Table[i][j - 1].H;
-            
-            Table[i][j].V %= MOD;
-            Table[i][j].H %= MOD;
+            Table[i][j].H = (Table[i][j].H + Table[i][j - 1].H) % MOD; 
         }
     }
     
-    return (Table[m - 1][n - 1].H + Table[m - 1][n - 1].V) % MOD;
+    return (Table.back().back().H + Table.back().back().V) % MOD;
 }
