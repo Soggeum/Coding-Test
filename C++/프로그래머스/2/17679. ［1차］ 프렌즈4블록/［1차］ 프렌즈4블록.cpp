861d2922e28c0c1403434bc8ade20414a0528660
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-08-13 20:05:34 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 0.32 ms, Memory: 4.7 MB -BaekjoonHub

--- C++/프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.cpp
+++ C++/프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.cpp
@@ -6,44 +6,46 @@ using namespace std;
 struct Node
 {
     char c;
-    bool flag;    
+    bool d;
 };
 
 int solution(int m, int n, vector<string> board) {
     vector<vector<Node>> Table(m, vector<Node>(n));
-    for (int i = 0; i < m ; i++)
+    for (int i = 0;i < m; i++)
     {
         for (int j = 0;j < n; j++)
         {
             Table[i][j].c = board[i][j];
+            Table[i][j].d = false;
         }
     }
     
-    bool wflag = false;
-    do {
-        wflag = false;
-        for (int i = 0; i < m - 1; i++)
+    bool bFlag = true;
+    while (bFlag)
+    {
+        bFlag = false;
+        for (int i = 1; i < m; i++)
         {
-            for (int j = 0;j < n - 1; j++)
+            for (int j = 1;j < n; j++)
             {
                 char c = Table[i][j].c;
-                if (c && c == Table[i][j + 1].c && c == Table[i + 1][j].c && c== Table[i + 1][j + 1].c)
+                if (c && c == Table[i][j - 1].c && c == Table[i - 1][j - 1].c && c == Table[i - 1][j].c)
                 {
-                    Table[i][j].flag = true;
-                    Table[i][j + 1].flag = true;
-                    Table[i + 1][j].flag = true;
-                    Table[i + 1][j + 1].flag = true;
+                    bFlag = true;
+                    Table[i][j].d = true;
+                    Table[i][j - 1].d = true;
+                    Table[i - 1][j].d = true;
+                    Table[i - 1][j - 1].d = true;
                 }
             }
         }
         
-        for (int j= 0; j < n; j++)
+        for (int j = 0;j < n; j++)
         {
             for (int i = m - 1; i >= 0; i--)
             {
-                if (Table[i][j].flag)
+                if (Table[i][j].d)
                 {
-                    wflag = true;
                     for (int k = i; k > 0; k--)
                     {
                         Table[k][j] = Table[k - 1][j];
@@ -52,16 +54,15 @@ int solution(int m, int n, vector<string> board) {
                     i++;
                 }
             }
+        }
     }
-    } while (wflag);
-    
     
     int answer = 0;
-    for (const vector<Node>& row : Table)
+    for (int i = 0; i < m; i++)
     {
-        for (const Node& n : row)
+        for (int j = 0;j < n; j++)
         {
-            if (n.c == 0)
+            if (Table[i][j].c == 0)
             {
                 answer++;
             }
