816327a5e7c7277197da155d88722ea3f032cd65
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-09-06 22:11:46 +0900

    [level 3] Title: 보행자 천국, Time: 22.59 ms, Memory: 7.7 MB -BaekjoonHub

--- C++/프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
+++ C++/프로그래머스/3/1832. 보행자 천국/보행자 천국.cpp
@@ -2,21 +2,17 @@
 
 using namespace std;
 
-int MOD = 20170805;
-
 struct Node
 {
-    int H, V;    
+    int V, H;    
 };
+
+int MOD = 20170805;
 
 // 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 int solution(int m, int n, vector<vector<int>> city_map) {
-    if (m * n == 1)
-    {
-        return 1;
-    }
     vector<vector<Node>> Table(m, vector<Node>(n));
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m ;i++)
     {
         if (city_map[i][0] == 1)
         {
@@ -33,26 +29,29 @@ int solution(int m, int n, vector<vector<int>> city_map) {
         Table[0][j].H = 1;
     }
     
-    for (int i = 1;i < m; i++){
-        for (int j = 1; j < n; j++)
+    for (int i = 1; i < m; i++)
+    {
+        for (int j = 1;j < n; j++)
         {
             if (city_map[i][j] == 1)
             {
                 continue;
             }
-                
+            Table[i][j].V = Table[i - 1][j].V;
             if (city_map[i - 1][j] != 2)
             {
-                Table[i][j].V = (Table[i][j].V + Table[i - 1][j].H) % MOD;    
+                Table[i][j].V += Table[i - 1][j].H;
             }
-            Table[i][j].V = (Table[i][j].V + Table[i - 1][j].V) % MOD;
+            Table[i][j].H = Table[i][j - 1].H;
             if (city_map[i][j - 1] != 2)
             {
-                Table[i][j].H = (Table[i][j].H + Table[i][j - 1].V) % MOD;    
+                Table[i][j].H += Table[i][j - 1].V;
             }
-            Table[i][j].H = (Table[i][j].H + Table[i][j - 1].H) % MOD; 
+            
+            Table[i][j].V %= MOD;
+            Table[i][j].H %= MOD;
         }
     }
     
-    return (Table.back().back().H + Table.back().back().V) % MOD;
+    return (Table[m - 1][n - 1].V + Table[m - 1][n - 1].H) % MOD;
 }
