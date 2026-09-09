cbd1713c6ed422dcc404c64708b892a8e79f31b6
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-09-09 18:17:02 +0900

    [level 4] Title: 호텔 방 배정, Time: 122.90 ms, Memory: 52.6 MB -BaekjoonHub

--- C++/프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.cpp
+++ C++/프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.cpp
@@ -4,23 +4,26 @@
 
 using namespace std;
 
-long long GetRoom(long long rn, unordered_map<long long, long long>& Table)
+long long GetRoomNumber(long long n, unordered_map<long long, long long>& um)
 {
-    if (Table.find(rn) == Table.end())
+    if (um.find(n) == um.end())
     {
-        Table[rn] = rn + 1;
-        return rn;
+        um[n] = n + 1;
+        return n;
     }
     
-    return Table[rn] = GetRoom(Table[rn], Table);
+    long long RoomNumber = GetRoomNumber(um[n], um);
+    um[n] = RoomNumber + 1;
+    return RoomNumber;
 }
 
 vector<long long> solution(long long k, vector<long long> room_number) {
-    unordered_map<long long, long long> Table;
     vector<long long> answer;
-    for (long long rn : room_number)
+    unordered_map<long long, long long> um;
+    for (long long n : room_number)
     {
-        answer.push_back(GetRoom(rn, Table));
+        answer.push_back(GetRoomNumber(n, um));
     }
+    
     return answer;
 }
