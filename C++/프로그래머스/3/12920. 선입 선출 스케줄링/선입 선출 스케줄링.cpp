8ee920e4120fb1cd2e028cc8ad3b89536f85c812
Soggeum <166914281+Soggeum@users.noreply.github.com>
2026-09-24 22:36:15 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 0.44 ms, Memory: 4.57 MB -BaekjoonHub

--- C++/프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.cpp
+++ C++/프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.cpp
@@ -4,41 +4,42 @@
 using namespace std;
 
 int solution(int n, vector<int> cores) {
-    long long Start = 1, End = n * 10000ll, answer = End;
+    long long Start = 1, End = 1ll * n * 10000, Time = End;
     while (Start <= End)
     {
-        long long Mid = (Start + End) / 2;
-        long long Count = 0;
-        for (int c : cores)
+        long long Mid = (Start + End) / 2, Cnt = 0;
+        for (int core : cores)
         {
-            Count += Mid / c + 1;
+            Cnt += Mid / core + 1;
         }
         
-        if (Count >= n)
+        if (Cnt >= n)
         {
-            answer = Mid;
+            Time = Mid;
             End = Mid - 1;
         }
         else
         {
-            Start  = Mid + 1;
+            Start = Mid + 1;
         }
     }
     
-    for (int c : cores)
+    for (int core : cores)
     {
-        n -= (answer - 1) / c + 1;        
-    } 
+        n -= (Time - 1) / core + 1;
+    }
+    
     for (int i = 0; i < cores.size(); i++)
     {
-        if (answer % cores[i] == 0)
+        if (Time % cores[i] == 0)
         {
             n--;
+            if (n == 0)
+            {
+                return i + 1;
+            }
         }
-        if (n == 0)
-        {
-            return i + 1;
-        }
-    } 
-    return cores.size();
+    }
+    
+    return -1;
 }
